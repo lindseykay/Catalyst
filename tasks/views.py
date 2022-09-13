af236054c9640fc9857ce2f79e50f8b11918d0ad
Lindsey <lindseykaycarlson@outlook.com>
2022-09-13 12:20:18 -0700

    Feature 16 completed -TaskListView

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -10,8 +11,16 @@ from tasks.models import Task
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
-    template_name = "new.html"
+    template_name = "tasks/new.html"
     fields = ["name", "start_date", "due_date", "project", "assignee"]
 
     def get_success_url(self) -> str:
         return reverse_lazy("show_project", args={self.object.project.id})
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "tasks/list.html"
+
+    def get_queryset(self):
+        return Task.objects.filter(assignee=self.request.user)
