09fbd586a7b88c00b3e56ee7f7a6aee184960fc5
Lindsey <lindseykaycarlson@outlook.com>
2022-09-13 12:44:39 -0700

    Feature 17 complete - TaskUpdateView

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -24,3 +24,10 @@ class TaskListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = "tasks/list.html"
+    fields = ["is_completed"]
+    success_url = reverse_lazy("show_my_tasks")
